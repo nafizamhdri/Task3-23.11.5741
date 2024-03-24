b1c8728a921dac2a2a5f21d939748684ce87fed9
nafizamhdri <nafizawae@students.amikom.ac.id>
2024-03-24 19:01:58 +0700

    Task3

---
+++ latihan class dan object/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace latihan_class_dan_object
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Taxi taxi = new Taxi();   
+
+            taxi.Name = "Jono";
+            taxi.OnDuty = true;
+            taxi.No = 10;
+
+            taxi.TaxiInfo();
+            taxi.PickUp();
+            taxi.DropOff();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ latihan class dan object/Taxi.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace latihan_class_dan_object
+{
+    public class Taxi
+    {
+        public string Name { get; set; }
+        public bool OnDuty { get; set; }    
+        public float No { get; set; }
+
+        public void TaxiInfo()
+        {
+            Console.WriteLine("Driver Name: {0}", Name);
+            Console.WriteLine("On Duty: {0}", (OnDuty ? "Yes" : "No"));
+            Console.WriteLine("Number of Passenger: {0}", No);
+        }
+        public void PickUp()
+        { Console.WriteLine("{0} Sedang menjemput penumpang", Name); }
+        public void DropOff()
+        {
+            Console.WriteLine("{0} Jono selesai menjemput penumpang\n", Name);        
+        }
+    }
+}
